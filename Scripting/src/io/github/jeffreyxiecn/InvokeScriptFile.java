2371433bf3f3be4133b98ac2bc0c5f78fce7aa30
Jeffrey XIE <xwy5201314@gmail.com>
2018-02-23 15:14:29 -0500

    Call JavaScript functions in a file

---
+++ Scripting/src/io/github/jeffreyxiecn/InvokeScriptFile.java
@@ -0,0 +1,52 @@
+package io.github.jeffreyxiecn;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+/*www.java2s.com*/
+public class InvokeScriptFile {
+  public static void main(String[] args) throws Exception {
+    System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+    ScriptEngineManager manager = new ScriptEngineManager();
+    ScriptEngine engine = manager.getEngineByName("JavaScript");
+    if (!(engine instanceof Invocable)) {
+      System.out.println("Invoking methods is not supported.");
+      return;
+    }
+
+    Invocable inv = (Invocable) engine;
+    String scriptPath = "src\\io\\github\\jeffreyxiecn\\calculator.js";
+    //String scriptPath = "calculator.js";
+    //URL fileUrl = getClass().getResource(scriptPath);
+
+    //engine.eval("load('" + scriptPath + "')");
+    engine.eval(Files.newBufferedReader(Paths.get(scriptPath), StandardCharsets.UTF_8));
+
+
+    Object calculator = engine.get("calculator");
+    //Object calculator = engine.get("nonExist");
+
+    int x = 3;
+    int y = 4;
+
+    for (int i = 0; i < 600; i++){
+	    Object addResult = inv.invokeMethod(calculator, "add", x, y);
+	    Object subResult = inv.invokeMethod(calculator, "subtract", x, y);
+	    Object mulResult = inv.invokeMethod(calculator, "multiply", x, y);
+	    Object divResult = inv.invokeMethod(calculator, "divide", x, y);
+
+	    System.out.println(addResult);
+	    System.out.println(subResult);
+	    System.out.println(mulResult);
+	    System.out.println(divResult);
+	    Thread.sleep(1000);
+    }
+
+    //Object triggerException = inv.invokeMethod(calculator, "nonexist", x, y);
+  }
+}
