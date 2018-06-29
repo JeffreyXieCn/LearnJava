e932726ff433d47f1b0b09fc069c2527034c68d1
Jeffrey XIE <xwy5201314@gmail.com>
2018-06-28 23:39:14 -0400

    Start learning Spring Framework

---
+++ SpringToolSuiteWS/MyJavaCode/src/io/github/jeffreyxiecn/multithreading/ExecutorServiceNumberOfThreads.java
@@ -0,0 +1,38 @@
+package io.github.jeffreyxiecn.multithreading;
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ExecutorServiceNumberOfThreads {
+    public static void main(String[] args) {
+        System.out.println("### Testing Executors.newFixedThreadPool()");
+        testNewFixedThreadPool();
+    }
+
+    private static void testNewFixedThreadPool() {
+        ThreadPoolExecutor poolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(100);
+        System.out.println("Pool when no execute() : " + poolExecutor.getPoolSize());
+        executeRunnable(poolExecutor);
+        System.out.println("Pool after 1st execute() : " + poolExecutor.getPoolSize());
+        executeRunnable(poolExecutor);
+        System.out.println("Pool after 2nd execute() : " + poolExecutor.getPoolSize());
+        executeRunnable(poolExecutor);
+        System.out.println("Pool after 3rd execute() : " + poolExecutor.getPoolSize());
+        executeRunnable(poolExecutor);
+        System.out.println("Pool after 4th execute() : " + poolExecutor.getPoolSize());
+        executeRunnable(poolExecutor);
+        System.out.println("Pool after 5th execute() : " + poolExecutor.getPoolSize());
+    }
+
+    private static void executeRunnable(ThreadPoolExecutor poolExecutor) {
+        poolExecutor.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("Running: " + Thread.currentThread().getId() + " | " + new Date());
+            }
+        });
+    }
+
+}
