25d407ce146e887cb4fc834dfea18e0bb4452ef1
Jeffrey XIE <xwy5201314@gmail.com>
2018-09-15 08:41:23 -0400

    Practice some algorithms

--- SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/algorithms/SortAlgorithms.java
+++ SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/algorithms/SortAlgorithms.java
@@ -1,19 +1,130 @@
 package io.github.jeffreyxiecn.algorithms;
 
+import java.util.Arrays;
+
 public class SortAlgorithms {
   
   public static void selectionSort(int[] input) {
-    int minIndex, temp;
-    for(int i = 0; i < input.length - 1; i++) {
+    int minIndex;
+    int temp;
+    int inputLength = input.length;
+    for (int i = 0; i < inputLength - 1; i++) {
       minIndex = i;
-      for(int j = i + 1; j < input.length; j++) {
-        if(input[j] < input[minIndex]) {
+      for (int j = i + 1; j < inputLength; j++) {
+        if (input[j] < input[minIndex]) {
           minIndex = j;
         }
       }
       temp = input[i];
       input[i] = input[minIndex];
       input[minIndex] = temp;
+    }
+  }
+  
+  public static void bubbleSort(int[] input) {
+    bubbleSort (input, true);
+  }
+
+  public static void bubbleSort(int[] input, boolean ascending) {
+    boolean swapped = true;
+    int inputLength = input.length;
+    int temp;
+    for (int i = 0; i < inputLength - 1 && swapped; i++) {
+      swapped = false;
+      for (int j = 1; j < inputLength - i; j++) {
+        if (ascending) {
+          if (input[j - 1] > input[j]) {
+            temp = input[j - 1];
+            input[j - 1] = input[j];
+            input[j] = temp;
+            swapped = true;
+          }
+        } else {
+          if (input[j - 1] < input[j]) {
+            temp = input[j - 1];
+            input[j - 1] = input[j];
+            input[j] = temp;
+            swapped = true;
+          }
+        }
+      }
+    }
+  }
+  
+  public static int[] moveZerosToArrayEnd(int[] input) {
+    //Arrays.sort(input);
+    Integer[] intArr = Arrays.stream(input).boxed().toArray(Integer[]::new);
+    Arrays.sort(intArr, (a, b) -> b == 0 ? -1 : 0); //treat 0 as positive infinity
+    //Arrays.sort(intArr, (a, b) -> b.intValue() == 0 ? -1 : 0);
+//    Arrays.sort(intArr, (a, b) -> {
+//      if (b == 0) {
+//        return -1;
+//      }
+//      else {
+//        return 0;
+//      }
+//     });
+    return Arrays.stream(intArr).mapToInt(Integer::intValue).toArray();
+    //Arrays.sort(intArr, (a, b) -> b -a);
+  }
+  
+  /**
+   * https://leetcode.com/problems/move-zeroes/description/#
+   * 
+   * @param nums
+   */
+  public static void moveZerosToArrayEnd2(int[] nums) {
+    if(nums.length < 2) {
+      return;
+    }
+    
+    int curZeroIdx = nums.length - 1;
+    int prevZeroIdx = curZeroIdx;
+    while (true) {
+      while (prevZeroIdx >= 0 && nums[prevZeroIdx] != 0 ) {
+        prevZeroIdx--;
+      }
+      if (prevZeroIdx < 0) {
+        // no more zero in the front of the array
+        break;
+      }
+      
+      //move the zero at prevZeroIdx to the end of the array
+      while (prevZeroIdx < curZeroIdx) {
+        nums[prevZeroIdx] = nums[prevZeroIdx + 1];
+        nums[prevZeroIdx + 1] = 0;
+        prevZeroIdx++;
+      }
+      curZeroIdx--;
+      prevZeroIdx--;
+    }
+  }
+  
+  public static void moveZerosToArrayEnd3(int[] nums) {
+    int lastNonZeroFoundAt = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        nums[lastNonZeroFoundAt++] = nums[i];
+      }
+    }
+    
+    for (int j = lastNonZeroFoundAt; j < nums.length; j++) {
+      nums[j] = 0;
+    }
+  }
+  
+  public static void moveZerosToArrayEnd4Best(int[] nums) {
+    //int temp;
+    for (int lastNonZeroFoundAt = 0, i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        //temp = nums[lastNonZeroFoundAt];
+        if(i != lastNonZeroFoundAt) {
+          nums[lastNonZeroFoundAt] = nums[i];
+          nums[i] = 0;
+        }
+        lastNonZeroFoundAt++;
+        //nums[i] = temp;
+      }
     }
   }
 }
--- SpringToolSuiteWS/my-java-code/src/test/java/io/github/jeffreyxiecn/algorithms/SortAlgorithmsTest.java
+++ SpringToolSuiteWS/my-java-code/src/test/java/io/github/jeffreyxiecn/algorithms/SortAlgorithmsTest.java
@@ -1,10 +1,15 @@
 package io.github.jeffreyxiecn.algorithms;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static org.hamcrest.CoreMatchers.equalTo;
 
 public class SortAlgorithmsTest {
   private static final Logger LOG = LoggerFactory.getLogger(SortAlgorithmsTest.class);
@@ -17,5 +22,94 @@ public class SortAlgorithmsTest {
     int[] output = {1, 5, 7, 8, 9, 10, 88, 99};
     SortAlgorithms.selectionSort(input);
     assertThat(input, equalTo(output));
+  }
+
+  @Test
+  public void testBubbleSort() {
+    LOG.debug("Test bubble sort");
+    int[] input = {10, 8, 99, 7, 1, 5, 88, 9};
+    LOG.debug("input.getClass() = " + input.getClass().getCanonicalName());
+    LOG.debug("input=" + input);
+    int[] input2 = input; // refer to the same array
+    int[] input3 = input.clone(); // clone to a new array with the same contents
+    int[] sortAsc = input.clone();
+    Arrays.sort(sortAsc);
+    SortAlgorithms.bubbleSort(input);
+    assertThat(input, equalTo(sortAsc));
+    LOG.debug("input2: " + Arrays.toString(input2));
+    LOG.debug("input3: " + Arrays.toString(input3));
+
+    List<Integer> expected = Arrays.stream(input3).boxed().collect(Collectors.toList());
+    Collections.sort(expected);
+    Collections.reverse(expected);
+    LOG.debug(expected.toString());
+    // LOG.debug("input3: " + Arrays.toString(input3));
+    // Integer[] expectedArr = expected.toArray(new Integer[0]);
+    int[] sortDesc = expected.stream().mapToInt(i -> i).toArray();
+    LOG.debug(Arrays.toString(sortDesc));
+    SortAlgorithms.bubbleSort(input3, false);
+    assertThat(input3, equalTo(sortDesc));
+
+    // List<Integer> list = Arrays.asList(input3);
+
+    // Integer[] sourceArray = { 0, 1, 2, 3, 4, 5 };
+    // List<Integer> targetList = Arrays.asList(sourceArray);
+
+    int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    //Collections.sort(data);
+    // To boxed array
+    Integer[] what = Arrays.stream(data).boxed().toArray(Integer[]::new);
+    Integer[] ever = IntStream.of(data).boxed().toArray(Integer[]::new);
+    //int[] back = Arrays.stream(what).mapToInt(i -> i).toArray();
+    int[] back = Arrays.stream(what).mapToInt(Integer::intValue).toArray();
+    LOG.debug(Arrays.toString(back));
+    //Collections.sort(what);
+    // To boxed list
+    List<Integer> you = Arrays.stream(data).boxed().collect(Collectors.toList());
+    List<Integer> like = IntStream.of(data).boxed().collect(Collectors.toList());
+    
+    int[] arr = {10, 8, 99, 7, 1, 5, 88, 9};
+    int[] expectedDescArr = {99, 88, 10, 9, 8, 7, 5, 1};
+    Integer[] intArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+    //Arrays.sort(intArr, Collections.reverseOrder());
+    Arrays.sort(intArr, (a, b) -> (b - a));
+    //Arrays.sort(intArr, (a, b) -> (b == 1 ? -1 : 0));
+    //Arrays.sort(intArr, (a, b) -> (a - b));
+    int[] descArr = Arrays.stream(intArr).mapToInt(Integer::intValue).toArray();
+    LOG.debug("descArr:" + Arrays.toString(descArr));
+    assertThat(descArr, equalTo(expectedDescArr));
+  }
+  
+  @Test
+  public void testMoveZerosToArrayEnd() {
+    int[] input1 = {3, 0, 4, 0, 1, 0, 2, 0};
+    int[] expected1 = {3, 4, 1, 2, 0, 0, 0, 0};
+    int[] result1 = SortAlgorithms.moveZerosToArrayEnd(input1);
+    if (new Integer(0) == 0) {
+      LOG.debug("new Integer(0) == 0");
+    }    
+    if (new Integer(0).intValue() == 0) {
+      LOG.debug("new Integer(0).intValue == 0");
+    }
+    assertThat(result1, equalTo(expected1));
+    SortAlgorithms.moveZerosToArrayEnd2(input1);
+    assertThat(input1, equalTo(expected1));
+    
+    int[] input2 = {0, 4, 0, 1, 0, 0, 2};
+    int[] input2a = input2.clone();
+    int[] expected2 = {4, 1, 2, 0, 0, 0, 0};
+    int[] result2 = SortAlgorithms.moveZerosToArrayEnd(input2);
+    assertThat(result2, equalTo(expected2));
+    SortAlgorithms.moveZerosToArrayEnd2(input2);
+    assertThat(input2, equalTo(expected2));
+    SortAlgorithms.moveZerosToArrayEnd3(input2a);
+    assertThat(input2a, equalTo(expected2));
+    
+    int[] input3 = {4, 1, 3, 2};
+    int[] expected3 = {4, 1, 3, 2};
+    int[] result3 = SortAlgorithms.moveZerosToArrayEnd(input3);
+    assertThat(result3, equalTo(expected3));
+    SortAlgorithms.moveZerosToArrayEnd2(input3);
+    assertThat(input3, equalTo(expected3));
   }
 }
