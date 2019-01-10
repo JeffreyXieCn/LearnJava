19157dd48f7895774697a741cab47b99cc084886
Jeffrey XIE <xwy5201314@gmail.com>
2019-01-09 22:20:50 -0500

    Finish AOP around advice

---
+++ SpringToolSuiteWS/spring-demo-aop-zz-around/src/com/luv2code/aopdemo/AfterFinallyDemoApp.java
@@ -0,0 +1,38 @@
+package com.luv2code.aopdemo;
+
+import java.util.List;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.luv2code.aopdemo.dao.AccountDAO;
+
+public class AfterFinallyDemoApp {
+
+  public static void main(String[] args) {
+    // read Spring config java class
+    AnnotationConfigApplicationContext context =
+        new AnnotationConfigApplicationContext(DemoConfig.class);
+
+    // get the bean from spring container
+    AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+    // call the business method
+    List<Account> theAccount = null;
+    try {
+      // add a boolean flag to simulate exceptions
+      boolean tripWire = true;
+      theAccount = theAccountDAO.findAccounts(tripWire);
+    } catch (Exception exc) {
+      System.out.println("\n\nMain Program ... caught exception: " + exc);
+    }
+
+    // display the accounts
+    System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+    System.out.println("----");
+    System.out.println(theAccount);
+    System.out.println("\n");
+
+    theAccountDAO.findAccounts(false);
+
+    // close the context
+    context.close();
+  }
+}
