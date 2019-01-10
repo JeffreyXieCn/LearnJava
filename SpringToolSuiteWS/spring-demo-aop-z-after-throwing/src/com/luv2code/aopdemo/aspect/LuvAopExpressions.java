0753efc40573e0a47cfd8cac6ff47fbceb04f232
Jeffrey XIE <xwy5201314@gmail.com>
2019-01-09 21:36:22 -0500

    Finish AOP after throwing

---
+++ SpringToolSuiteWS/spring-demo-aop-z-after-throwing/src/com/luv2code/aopdemo/aspect/LuvAopExpressions.java
@@ -0,0 +1,24 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class LuvAopExpressions {
+
+  // define Pointcut expressions for reuse
+  @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+  public void forDaoPackage() {}
+
+  // create Pointcut for getter methods
+  @Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+  public void getter() {}
+
+  // create Pointcut for setter methods
+  @Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+  public void setter() {}
+
+  // create Pointcut: include package ... exclude getter/setter
+  @Pointcut("forDaoPackage() && !(getter() || setter())")
+  public void forDaoPackageNoGetterSetter() {}
+}
