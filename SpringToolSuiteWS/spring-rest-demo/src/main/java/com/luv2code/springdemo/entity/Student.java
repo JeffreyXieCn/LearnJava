9c2af598ba6f57bdb3746b1455b5f97ffbef5d60
Jeffrey XIE <xwy5201314@gmail.com>
2019-01-18 21:26:18 -0500

    Work on spring rest demo with students api

---
+++ SpringToolSuiteWS/spring-rest-demo/src/main/java/com/luv2code/springdemo/entity/Student.java
@@ -0,0 +1,19 @@
+package com.luv2code.springdemo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Student {
+  private String firstName;
+  private String lastName;
+}
