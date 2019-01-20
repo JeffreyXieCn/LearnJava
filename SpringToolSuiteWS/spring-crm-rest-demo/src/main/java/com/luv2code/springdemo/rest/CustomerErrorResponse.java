e4c4c68bfa8c5aac94c5cbf4e7efab198841261f
Jeffrey XIE <xwy5201314@gmail.com>
2019-01-19 21:29:19 -0500

    Finish Spring CRM REST demo

---
+++ SpringToolSuiteWS/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerErrorResponse.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerErrorResponse {
+  private int status;
+  private String message;
+  private long timeStamp;
+}
