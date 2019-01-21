eca1c7a5b77009146fb7a898e60d6080f04b0dd1
Jeffrey XIE <xwy5201314@gmail.com>
2019-01-21 11:23:07 -0500

    Finish Spring Boot CRUD with spring data

---
+++ SpringToolSuiteWS/08-spring-boot-crud-springdata/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.cruddemo.service;
+
+import java.util.List;
+import java.util.Optional;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+  List<Employee> findAll();
+
+  Optional<Employee> findById(int theId);
+
+  void save(Employee theEmployee);
+
+  void deleteById(int theId);
+}
