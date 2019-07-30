f940aff2638c2d616ddee0eb66a882e2b5dd4f33
Jeffrey XIE <xwy5201314@gmail.com>
2019-07-30 08:59:14 -0400

    Revisit string permutation and subset generation

--- SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/algorithms/recursionanddp/PermutationGenerator.java
+++ SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/algorithms/recursionanddp/PermutationGenerator.java
@@ -8,21 +8,26 @@ public class PermutationGenerator {
   public static void main(String[] args) {
     System.out.println(permutationOf("ab"));
     System.out.println(permutationOf("abc"));
-    System.out.println(permutationOf("abcd"));
-    System.out.println(permutationOf("abcde"));
+    System.out.println(permutationOf("abcd")); // 4! = 4 * 3 * 2 * 1 = 24
+    System.out.println(permutationOf("abcde")); // 5! = 5 * 4 * 3 * 2 * 1 = 120
 
     System.out.println("==================");
     System.out.println(permutationOfK("ab", 1));
     System.out.println(permutationOfK("abc", 2));
-    System.out.println(permutationOfK("abcd", 2));
-    System.out.println(permutationOfK("abcde", 3));
+    System.out.println(permutationOfK("abcd", 2)); // N4K2 = 4 * 3 = 12
+    System.out.println(permutationOfK("abcde", 3)); // N5K3 = 5 * 4 * 3 = 60
 
     System.out.println("==================");
     System.out.println(subsetOfK("ab", 1));
+    System.out.println(subsetOfK2("ab", 1));
     System.out.println(subsetOfK("abc", 2));
+    System.out.println(subsetOfK2("abc", 2));
     System.out.println(subsetOfK("abcd", 2));
+    System.out.println(subsetOfK2("abcd", 2));
     System.out.println(subsetOfK("abcde", 3));
+    System.out.println(subsetOfK2("abcde", 3));
     System.out.println(subsetOfK("abcde", 5));
+    System.out.println(subsetOfK2("abcde", 5));
   }
 
   public static List<String> permutationOf(String input) {
@@ -31,12 +36,27 @@ public class PermutationGenerator {
     return result;
   }
 
+  /**
+   * P(abcd, 2) = aP(bcd, 1) + bP(acd, 1) + cP(abd, 1) + dP(abc, 1)
+   *
+   * @param input
+   * @param k
+   * @return
+   */
   public static List<String> permutationOfK(String input, int k) {
     List<String> result = new ArrayList<>();
     generatePermutations("", input, k, result);
     return result;
   }
 
+  /**
+   * P(abc) = aP(bc) + bP(ac) + cP(ab) = abP(c) + acP(b) + baP(c) + bcP(a) + caP(b) + cbP(a)
+   *
+   * @param prefix
+   * @param str
+   * @param k
+   * @param result
+   */
   private static void generatePermutations(String prefix, String str, int k, List<String> result) {
     if (prefix.length() == k) {
       result.add(prefix);
@@ -49,6 +69,14 @@ public class PermutationGenerator {
     }
   }
 
+  /**
+   * S(abcd, 2) = aS(bcd, 1) + bS(cd, 1) + cS(d, 1) S(abcde, 3) = aS(bcde, 2) + bS(cde, 2) + cS(de,
+   * 2)
+   *
+   * @param input
+   * @param k
+   * @return
+   */
   public static List<String> subsetOfK(String input, int k) {
     List<String> result = new ArrayList<>();
     generateSubsets("", input, k, result);
@@ -66,6 +94,26 @@ public class PermutationGenerator {
       char ch = str.charAt(i);
       String remain = str.substring(i + 1);
       generateSubsets(prefix + ch, remain, k, result);
+    }
+  }
+
+  public static List<String> subsetOfK2(String input, int k) {
+    List<String> result = new ArrayList<>();
+    generateSubsets2("", input, k, result);
+    return result;
+  }
+
+  private static void generateSubsets2(String prefix, String str, int k, List<String> result) {
+    if (k == 0) {
+      result.add(prefix);
+      return;
+    }
+
+    int end = str.length() - k;
+    for (int i = 0; i <= end; i++) {
+      char ch = str.charAt(i);
+      String remain = str.substring(i + 1);
+      generateSubsets2(prefix + ch, remain, k - 1, result); // can't replace k - 1 with --k
     }
   }
 }
