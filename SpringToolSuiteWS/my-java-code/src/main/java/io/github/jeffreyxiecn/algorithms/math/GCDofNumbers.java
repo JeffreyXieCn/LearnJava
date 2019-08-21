e9bfeb6fe6ead6bed7f60ac6a7bd100106a9ae3e
Jeffrey XIE <xwy5201314@gmail.com>
2019-08-21 07:50:13 -0400

    Improve the performance of calculating GCD by checking each bigger divisor up front

--- SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/algorithms/math/GCDofNumbers.java
+++ SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/algorithms/math/GCDofNumbers.java
@@ -1,6 +1,8 @@
 package io.github.jeffreyxiecn.algorithms.math;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -59,5 +61,59 @@ public class GCDofNumbers {
     // Collections.reverse(smaller);
     bigger.addAll(smaller);
     return bigger;
+  }
+
+  public static int generalizedGCD2(int num, int[] arr) {
+    int min = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+
+    Deque<Integer> divisorsStack = new ArrayDeque<>();
+    int sqrt = (int) Math.sqrt(min);
+    int end;
+    if (sqrt * sqrt == min) {
+      end = sqrt - 1;
+    } else {
+      end = sqrt;
+    }
+
+    int divisor;
+    for (int i = 1; i <= end; i++) {
+      if (min % i == 0) {
+        divisor = min / i;
+        if (canDivideBy(arr, divisor)) {
+          return divisor;
+        }
+        // check the smaller divisors after checking all the bigger ones;
+        divisorsStack.push(i);
+      }
+    }
+
+    if (end == sqrt - 1) {
+      // smaller.add(sqrt); // add it only once
+      divisorsStack.push(sqrt); // or bigger.add(sqrt);
+    }
+
+    while (!divisorsStack.isEmpty()) {
+      // check from bigger divisor to smaller divisor
+      divisor = divisorsStack.pop();
+      if (canDivideBy(arr, divisor)) {
+        return divisor;
+      }
+    }
+
+    return 1;
+  }
+
+  private static boolean canDivideBy(int[] arr, int divisor) {
+    for (int e : arr) {
+      if (e % divisor != 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
--- SpringToolSuiteWS/my-java-code/src/test/java/io/github/jeffreyxiecn/algorithms/math/GCDofNumbersTest.java
+++ SpringToolSuiteWS/my-java-code/src/test/java/io/github/jeffreyxiecn/algorithms/math/GCDofNumbersTest.java
@@ -24,4 +24,36 @@ class GCDofNumbersTest {
     int actual = GCDofNumbers.generalizedGCD(arr.length, arr);
     assertEquals(expected, actual);
   }
+
+  @Test
+  void whenGCDisNotOne_thenReturnCorrectGCD2() {
+    int[] arr = {2, 4, 6, 8, 10};
+    int expected = 2;
+    int actual = GCDofNumbers.generalizedGCD2(arr.length, arr);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void whenGCDisOne_thenReturnOne2() {
+    int[] arr = {2, 3, 4, 5, 6};
+    int expected = 1;
+    int actual = GCDofNumbers.generalizedGCD2(arr.length, arr);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void whenGCDisBiggerDivisor_thenReturnCorrectGCD() {
+    int[] arr = {20, 30, 10, 50, 25};
+    int expected = 5;
+    int actual = GCDofNumbers.generalizedGCD2(arr.length, arr);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void whenGCDisSmallerDivisor_thenReturnCorrectGCD() {
+    int[] arr = {20, 30, 10, 50, 24};
+    int expected = 2;
+    int actual = GCDofNumbers.generalizedGCD2(arr.length, arr);
+    assertEquals(expected, actual);
+  }
 }
