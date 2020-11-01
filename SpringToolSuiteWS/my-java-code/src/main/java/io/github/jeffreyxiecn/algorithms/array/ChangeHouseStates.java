f22d7819123589093ac89b1a885cb476099a264a
Jeffrey XIE <xwy5201314@gmail.com>
2020-11-01 11:11:22 -0500

    Improve solution for array type question ChangeHouseStates

--- SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/algorithms/array/ChangeHouseStates.java
+++ SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/algorithms/array/ChangeHouseStates.java
@@ -1,5 +1,6 @@
 package io.github.jeffreyxiecn.algorithms.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 // IMPORT LIBRARY PACKAGES NEEDED BY YOUR PROGRAM
 import java.util.List;
@@ -7,17 +8,36 @@ import java.util.stream.Collectors;
 // SOME CLASSES WITHIN A PACKAGE MAY BE RESTRICTED
 // DEFINE ANY CLASS AND METHOD NEEDED
 // CLASS BEGINS, THIS CLASS IS REQUIRED
+/*
+ * given a cell, if the neighbours are different, then it becomes 1 the next day, otherwise, it
+ * becomes 0. Assume the virtual left most and right most neighbour is 0
+ */
 public class ChangeHouseStates {
   // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+  private int n;
+
   public List<Integer> cellCompete(int[] states, int days) {
     // WRITE YOUR CODE HERE
-    if (states.length < 2) {
+    if (states == null || states.length == 0) {
+      return new ArrayList<>();
+    }
+
+    if (days < 1) {
+      return Arrays.stream(states).boxed().collect(Collectors.toList());
+    }
+
+    n = states.length;
+    if (n == 1) {
       return Arrays.asList(0);
     }
 
-    int[] newState = states;
+    int[] newStates = new int[n];
+    int[] temp;
     for (int i = 1; i <= days; i++) {
-      newState = changeState(newState);
+      changeState(states, newStates);
+      temp = states;
+      states = newStates;
+      newStates = temp;
     }
 
     //    List<Integer> result = new ArrayList<>();
@@ -25,23 +45,20 @@ public class ChangeHouseStates {
     //      result.add(element);
     //    }
 
-    return Arrays.stream(newState).boxed().collect(Collectors.toList());
+    return Arrays.stream(states).boxed().collect(Collectors.toList());
     //    return result;
   }
   // METHOD SIGNATURE ENDS
 
-  private int[] changeState(int[] states) {
-    int[] newState = new int[states.length];
+  private void changeState(int[] states, int[] newStates) {
     // newState[0] = states[1] == 1 ? 1 : 0;
-    newState[0] = states[1] ^ 0;
+    newStates[0] = states[1] ^ 0;
     // newState[states.length - 1] = states[states.length - 2] == 1 ? 1 : 0;
-    newState[states.length - 1] = states[states.length - 2] ^ 0;
+    newStates[n - 1] = states[n - 2] ^ 0;
 
-    for (int i = 1; i < states.length - 1; i++) {
+    for (int i = 1; i < n - 1; i++) {
       // newState[i] = states[i - 1] == states[i + 1] ? 0 : 1;
-      newState[i] = states[i - 1] ^ states[i + 1];
+      newStates[i] = states[i - 1] ^ states[i + 1];
     }
-
-    return newState;
   }
 }
