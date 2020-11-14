06267ed3b735d0996ab02272add4ed50bfc0457c
Jeffrey XIE <xwy5201314@gmail.com>
2020-11-14 11:00:21 -0500

    Split string into tokens

--- SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/string/StringManipulation.java
+++ SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/string/StringManipulation.java
@@ -8,6 +8,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -21,9 +22,24 @@ public class StringManipulation {
   }
 
   public static void main(String[] args) {
-    String token = "abcd1234";
-    String[] tokens = token.split(" ");
-    System.out.println("tokens:" + tokens.length);
+    String token1 = "abcd1234";
+    String[] tokens1 = token1.split(" ");
+    System.out.println("tokens1:" + tokens1.length); // 1
+
+    String token2 = "1.2.3";
+    String[] tokens2 = token2.split(".");
+    System.out.println("tokens2:" + tokens2.length); // 0
+
+    String[] tokens3 = token2.split("\\.");
+    System.out.println("tokens3:" + tokens3.length); // 3
+
+    List<String> setTokens =
+        Collections.list(new StringTokenizer("1.2.3", "."))
+            .stream()
+            .map(token -> (String) token)
+            .collect(Collectors.toList());
+
+    System.out.println("setTokens:" + setTokens);
 
     BodyType bodyType = BodyType.MULTIPART;
     System.out.println("Body Type: " + bodyType);
