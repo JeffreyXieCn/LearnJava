c42b1c514e2ffc9172ef4e3c90f41b9f28bdd087
Jeffrey XIE <xwy5201314@gmail.com>
2021-07-23 10:52:01 -0400

    Revisit Java basic data types

--- SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/datatypes/BasicDataType.java
+++ SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/datatypes/BasicDataType.java
@@ -4,33 +4,50 @@ import java.nio.ByteOrder;
 
 public class BasicDataType {
 
-  public static void main(String[] args) {
-    ByteOrder byteOrder = ByteOrder.nativeOrder();
-    if (byteOrder.equals(ByteOrder.LITTLE_ENDIAN)) {
-      System.out.println("This JVM is running on little_endian platform");
-    } else {
-      System.out.println("This JVM is running on big_endian platform");
+    public static void main(String[] args) {
+        ByteOrder byteOrder = ByteOrder.nativeOrder();
+        if (byteOrder.equals(ByteOrder.LITTLE_ENDIAN)) {
+            System.out.println("This JVM is running on little_endian platform");
+        } else {
+            System.out.println("This JVM is running on big_endian platform");
+        }
+
+        int a = 15;
+        System.out.println(Integer.toString(a));
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toOctalString(a));
+        System.out.println(Integer.toHexString(a));
+        System.out.println(String.valueOf(a));
+
+        System.out.println(String.valueOf('a'));
+
+        System.out.println(Integer.parseInt("-123"));
+        System.out.println(Integer.valueOf("-123"));
+
+        System.out.println("a".charAt(0));
+
+        Integer aa = a;
+        System.out.println(aa.toString());
+        System.out.println(new Integer(a));
+        System.out.println(Integer.valueOf(a));
+
+        System.out.println(aa.intValue());
+
+        long creditCardNumber = 1234_5678_9012_3456L;
+        System.out.println("creditCardNumber:" + creditCardNumber);
+        long socialSecurityNumber = 999_99_9999L;
+        float pi = 3.14_15F;
+        long hexBytes = 0xFF_EC_DE_5E;
+        long hexWords = 0xCAFE_BABE;
+        long maxLong = 0x7fff_ffff_ffff_ffffL;
+        System.out.println("maxLong:" + maxLong); // 9223372036854775807
+        long minLong = 0x8000_0000_0000_0000L;
+        System.out.println("minLong:" + minLong); // -9223372036854775808
+        System.out.println(Long.toHexString(Long.MIN_VALUE));
+        byte nybbles = 0b0010_0101;
+        System.out.println("nybbles:" + nybbles); // 37
+        long bytes = 0b11010010_01101001_10010100_10010010;
+        System.out.println("bytes:" + bytes); // -764832622
+
     }
-
-    int a = 15;
-    System.out.println(Integer.toString(a));
-    System.out.println(Integer.toBinaryString(a));
-    System.out.println(Integer.toOctalString(a));
-    System.out.println(Integer.toHexString(a));
-    System.out.println(String.valueOf(a));
-
-    System.out.println(String.valueOf('a'));
-
-    System.out.println(Integer.parseInt("-123"));
-    System.out.println(Integer.valueOf("-123"));
-
-    System.out.println("a".charAt(0));
-
-    Integer aa = a;
-    System.out.println(aa.toString());
-    System.out.println(new Integer(a));
-    System.out.println(Integer.valueOf(a));
-
-    System.out.println(aa.intValue());
-  }
 }
