9d9517eaae985cc76cb6eca39b81a18401415311
Jeffrey XIE <xwy5201314@gmail.com>
2024-04-02 21:33:55 -0400

    Rename this Java application

--- SpringToolSuiteWS/06-spring-boot-cruddemo-H2DB/src/main/java/com/luv2code/springboot/cruddemo/CrudDemoApplication.java
+++ SpringToolSuiteWS/06-spring-boot-cruddemo-H2DB/src/main/java/com/luv2code/springboot/cruddemo/CrudDemoApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CruddemoApplication {
+public class CrudDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudDemoApplication.class, args);
+    }
 
 }
 
