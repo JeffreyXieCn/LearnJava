fdea0e510bc72ccbb09ec9cc71a8a44b39ca23ec
Jeffrey XIE <wenyan.a.xie@ericsson.com>
2024-12-23 16:23:57 -0500

    Task is removed from the ArrayBockingQueue when it starts executing in a thread

--- SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/multithreading/ParallelTaskExecutorWithLatch.java
+++ SpringToolSuiteWS/my-java-code/src/main/java/io/github/jeffreyxiecn/multithreading/ParallelTaskExecutorWithLatch.java
@@ -6,6 +6,7 @@ public class ParallelTaskExecutorWithLatch {
     public static void main(String[] args) {
         // Create a fixed thread pool of size 5
         ExecutorService executorService = Executors.newFixedThreadPool(5);
+        System.out.println("executorService: " + executorService);
 
         // CountDownLatch to track 20 tasks
         CountDownLatch latch = new CountDownLatch(20);
@@ -14,6 +15,7 @@ public class ParallelTaskExecutorWithLatch {
             for (int i = 1; i <= 20; i++) {
                 int taskId = i;
                 executorService.submit(() -> {
+                    System.out.println("1.executorService: " + executorService);
                     try {
                         System.out.println("Executing task " + taskId + " by " + Thread.currentThread().getName());
                         Thread.sleep(1000); // Simulate task work
@@ -21,14 +23,16 @@ public class ParallelTaskExecutorWithLatch {
                         Thread.currentThread().interrupt();
                     } finally {
                         latch.countDown(); // Decrement latch count when task completes
+                        System.out.println("2.executorService: " + executorService);
                     }
                 });
             }
+            System.out.println("3.executorService: " + executorService);
 
             // Wait for all tasks to finish
             latch.await();
-
             System.out.println("All tasks completed.");
+            System.out.println("4.executorService: " + executorService);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
